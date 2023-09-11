1386752d869a6da1291fa9832b8e09a8bda889eb
jabka <cyberjabka1@icloud.com>
2023-09-11 15:08:24 +0300

    ssh connection core

---
+++ connections_manager.py
@@ -0,0 +1,25 @@
+import asyncio
+import asyncssh
+
+async def ssh_client(hostname, port, username, password):
+    try:
+        async with asyncssh.connect(hostname, port=port, username=username, password=password, known_hosts=None) as ssh:
+            while True:
+                command = await asyncio.to_thread(input, f"{username}$ ")
+                if command.lower() == "exit":
+                    break
+
+                _, stdout, _ = await ssh.open_session(command)
+                result = await stdout.read()
+                print(result)
+    except Exception as e:
+        print("Ошибка: ", str(e))
+
+
+if __name__ == "__main__":
+    hostname = input("Введите имя хоста: ")
+    port = int(input("Введите порт (по умолчанию 22): ") or 22)
+    username = input("Введите имя пользователя: ")
+    password = input("Введите пароль: ")
+
+    asyncio.run(ssh_client(hostname, port, username, password))
