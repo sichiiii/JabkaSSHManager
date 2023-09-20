09c078cf85ea7f4329f0fcf32fa5bc85b076bb5c
sichiiii <cyberjabka1@icloud.com>
2023-09-20 13:49:43 +0300

    login form

---
+++ ssh_manager/ssh_connections_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+    email = forms.CharField(max_length=65)
+
--- ssh_manager/ssh_manager/settings.py
+++ ssh_manager/ssh_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ssh_connections_app.apps.SshConnectionsAppConfig'
 ]
 
 MIDDLEWARE = [
