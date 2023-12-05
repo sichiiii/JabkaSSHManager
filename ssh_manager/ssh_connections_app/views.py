d16a1c4d5fe0e26e3213c827164efa3e462379ad
sichiiii <kirillfisyukovich@gmail.com>
2023-12-05 17:38:40 +0300

    Views fix
    Signed-off-by: sichiiii <kirillfisyukovich@gmail.com>

--- ssh_manager/ssh_connections_app/views.py
+++ ssh_manager/ssh_connections_app/views.py
@@ -33,8 +33,22 @@ def sign_out(request):
 
 
 def sign_up(request):
-    pass
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'sign_up.html', {'form': form})
 
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return 'True'
+
+        return render(request, 'sign_up.html', {'form': form})
 
 @login_required
 def create_ssh_connection(request):
