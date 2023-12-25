b0ee48090cdd293f846724f2a60ef7d85e303ecc
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2023-12-25 23:00:02 +0300

    Views additional functional
    Signed-off-by: cyberjabka <76742792+sichiiii@users.noreply.github.com>

--- ssh_manager/ssh_connections_app/forms.py
+++ ssh_manager/ssh_connections_app/forms.py
@@ -10,3 +10,11 @@ class SignUpForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
     email = forms.CharField(max_length=65)
+
+
+class SSHConnectionForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    host = forms.CharField(max_length=255)
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+    port = forms.IntegerField()
+
--- ssh_manager/ssh_connections_app/views.py
+++ ssh_manager/ssh_connections_app/views.py
@@ -1,29 +1,34 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
 
-from .forms import SignInForm, SignUpForm
+
+from .forms import SignInForm, SignUpForm, SSHConnectionForm
 
 
 def sign_in(request):
-    if request.method == 'GET':
-        form = SignInForm()
-        return render(request, 'sign_in.html', {'form': form})
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            form = SignInForm()
+            return render(request, 'sign_in.html', {'form': form})
 
-    elif request.method == 'POST':
-        form = SignInForm(request.POST)
+        elif request.method == 'POST':
+            form = SignInForm(request.POST)
 
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                return 'True'
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    login(request, user)
+                    return 'True'
 
-        messages.error(request, f'Invalid username or password')
-        return render(request, 'sign_in.html', {'form': form})
+            messages.error(request, f'Invalid username or password')
+            return render(request, 'sign_in.html', {'form': form})
+    else:
+        return render(request, 'error.html', {'error': 'You are logged in'})
 
 
 @login_required
@@ -33,37 +38,44 @@ def sign_out(request):
 
 
 def sign_up(request):
-    if request.method == 'GET':
-        form = SignUpForm()
-        return render(request, 'sign_up.html', {'form': form})
+    if not request.user.is_authenticated:
+        if request.method == 'GET':
+            form = SignUpForm()
+            return render(request, 'sign_up.html', {'form': form})
 
-    elif request.method == 'POST':
-        form = SignUpForm(request.POST)
+        elif request.method == 'POST':
+            form = SignUpForm(request.POST)
 
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                return 'True'
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
 
-        return render(request, 'sign_up.html', {'form': form})
+                user, created = User.objects.get_or_create(username=username, password=password)
+                if created:
+                    render(request, 'sign_up_success.html')
+                else:
+                    return render(request, 'error.html', {'error': 'User already exists'})
+
+            return render(request, 'sign_up.html', {'form': form})
+    else:
+        return render(request, 'error.html', {'error': 'You are already logged in'})
 
 
 @login_required
 def create_ssh_connection(request):
-    pass
+    form = SSHConnectionForm()
+    return render(request, 'create_ssh_connection.html', {'form': form})
 
 
 @login_required
 def edit_ssh_connection(request):
-    pass
+    form = SSHConnectionForm()
+    return render(request, 'edit_ssh_connection.html', {'form': form})
 
 
 @login_required
 def delete_ssh_connection(request):
-    pass
+    return render(request, 'delete_ssh_connection.html')
 
 
 @login_required
